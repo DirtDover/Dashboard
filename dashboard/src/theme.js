45e7607e8e34d36086403ba369ccbb44839d8c42
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-04-25 09:59:13 +0200

    Dark and light mis en place

--- dashboard/src/theme.js
+++ dashboard/src/theme.js
@@ -193,3 +193,26 @@ export const themeSettings = (mode) => {
        } 
     };
 };
+
+
+// context for color mode
+
+export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState("dark");
+
+    const colorMode = useMemo(
+       () => ({
+        toggleColorMode: () =>
+          setMode((pre) => (prev === "light" ? "dark" : "light"))
+       }),
+    []
+  );
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
+}
+
